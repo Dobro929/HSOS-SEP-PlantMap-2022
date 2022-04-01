8e0647c93b785178c5930734e007645f199af5de
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-04-01 16:46:17 +0200

    add vue & django

--- .devcontainer/installDependencies.sh
+++ .devcontainer/installDependencies.sh
@@ -119,7 +119,7 @@ apt-get update -qq && apt-get install -y -qq \
     nodejs \
     npm
 
-npm install vue
+npm install vue@next
 
 # remove apt lists so that they are not saved in the image layers
 rm -rf /var/lib/apt/lists/*

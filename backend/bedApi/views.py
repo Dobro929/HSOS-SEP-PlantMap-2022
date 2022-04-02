0d37e528e22990ca1aa57fd8031cd4d89d58cde9
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-04-02 16:38:08 +0200

    add beds & bed_detail

--- backend/bedApi/views.py
+++ backend/bedApi/views.py
@@ -1,16 +1,30 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# getter
+# Get beds
+@api_view(['GET'])
 def beds(request):
-    if request.method == 'GET':
-        return JsonResponse(
+    return JsonResponse(
+        {
+            "beds": [
+                "1",
+                "2",
+                "3",
+                "4",
+            ]
+        }
+    )
+
+
+# Get bed details
+@api_view(['GET'])
+def bed_detail(request, bed_id):
+    return JsonResponse(
             {
-                "beds": [
-                    "1",
-                    "2",
-                    "3",
-                    "4",
-                ]
+                "id": bed_id,
+                "name": "Westerberg",
+                "type": "strawberry",
+                "longitude": 52.0,
+                "latitude": 8.05
             }
         )

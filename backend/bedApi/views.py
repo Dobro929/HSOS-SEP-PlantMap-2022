1ba03b206e3f1a759bd0034b3e1f38a8b02e7129
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-04-04 22:34:51 +0200

    pre-commit changes

--- backend/bedApi/views.py
+++ backend/bedApi/views.py
@@ -20,26 +20,8 @@ def beds(request):
 @api_view(['GET'])
 def bed_detail(request, bed_id):
     beds = [
-        {
-            "id":1,
-            "name":"Westerberg",
-            "culture":"strawberry",
-            "longitude":52.28264,
-            "latitude":8.0278
-        },
-        {
-            "id":2,
-            "name":"Wüste",
-            "culture":"tomatoe",
-            "longitude":52.26564,
-            "latitude":8.0298
-        },
-        {
-            "id":3,
-            "name":"Hellern",
-            "culture":"lettuce",
-            "longitude":52.25564,
-            "latitude":7.99
-        }
-        ]
-    return JsonResponse(beds[bed_id-1])
+        {"id": 1, "name": "Westerberg", "culture": "strawberry", "longitude": 52.28264, "latitude": 8.0278},
+        {"id": 2, "name": "Wüste", "culture": "tomatoe", "longitude": 52.26564, "latitude": 8.0298},
+        {"id": 3, "name": "Hellern", "culture": "lettuce", "longitude": 52.25564, "latitude": 7.99},
+    ]
+    return JsonResponse(beds[bed_id - 1])

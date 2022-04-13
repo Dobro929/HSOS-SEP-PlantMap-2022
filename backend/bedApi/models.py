5c00a1968f442fb233dddfcf0fd89b11c6b5366d
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-04-13 19:01:26 +0200

    model changes

--- backend/bedApi/models.py
+++ backend/bedApi/models.py
@@ -2,11 +2,41 @@ import uuid
 
 from django.db import models
 
+# GEOLOCATION
+class Geolocation(models.Model):
+    coordinateSystem = models.CharField(max_length=100)
+    ellipsoid = models.CharField(max_length=100)
+    latitude =  models.FloatField()
+    longitude = models.FloatField()
+
+    # ToString
+    def __str__(self):
+        return self.latitude
+
 
 # BED
 class Bed(models.Model):
-    uuid = models.UUIDField()
+    uuid = models.CharField(primary_key=True, max_length=100)
     name = models.CharField(max_length=100)
+    geolocation = models.OneToOneField(
+        Geolocation,
+        on_delete=models.CASCADE,
+        #primary_key=True,
+    )
+
+    # ToString
+    def __str__(self):
+        return self.name
+
+
+# GEOMETRIE
+class Geometrie(models.Model):
+    name = models.CharField(max_length=100)
+    uuid = models.CharField(primary_key= True, max_length=100)
+    type =  models.CharField(max_length=100)
+    frameId = models.CharField(max_length=100)
+    stamp = models.CharField(max_length=100)
+    bed = models.ForeignKey(Bed, on_delete=models.CASCADE)
 
     # ToString
     def __str__(self):
